a57919b231ae45f1a776346008896467e0f813d2
Ryan Thommes <rthommes773@gmail.com>
2022-01-18 18:16:02 -0600

    first p5 sketch

---
+++ sketch.js
@@ -0,0 +1,14 @@
+let gif_hotdog;
+
+function preload() {
+    gif_hotdog = createImg('assets/hotdog.gif', 500, 500);
+}
+
+function setup() {
+    //createCanvas(500, 700);
+}
+
+function draw() {
+    //background(0);
+    gif_hotdog.center();
+}
